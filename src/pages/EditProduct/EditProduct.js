a8de1e30eb5d2c0111144a9ae2f5c87317d0f10c
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-06-19 22:03:03 +0700

    siuuuuuuuuu

--- src/pages/EditProduct/EditProduct.js
+++ src/pages/EditProduct/EditProduct.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import {MdAdd} from 'react-icons/md';
+
+import styles from './EditProduct.module.scss';
+import classNames from 'classnames/bind';
 import * as ProductServices from '~/services/ProductServices';
 import error_image from '~/assets/image_erorr.jpg';
 
+const cx = classNames.bind(styles);
 function EditProduct() {
     const [name, setName] = useState('');
     const [price, setPrice] = useState(0);
@@ -92,7 +97,7 @@ function EditProduct() {
         setCategory([...category, '']);
     };
 
-    const handleCategoryInputChange = (index, e) => {
+    const handleCategorySelectChange = (index, e) => {
         const updatedCategory = [...category];
         updatedCategory[index] = e.target.value;
         setCategory(updatedCategory);
@@ -109,62 +114,83 @@ function EditProduct() {
     };
 
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <h2>Chỉnh sửa sản phẩm</h2>
-            <label>
-                Tên sản phẩm:
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
-                Giá:
-                <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
-            </label>
-            <label>
-                Số lượng:
-                <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-            </label>
-            <label>
-                Mô tả:
-                <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-            </label>
-            <label>
-                Ảnh:
-                <div>
-                    {images.map((image, index) => (
-                        <div key={index}>
-                            <input
-                                type="text"
-                                value={image}
-                                onChange={(e) => handleImageInputChange(index, e)}
-                            />
-                            <img src={image} alt={`Product ${index + 1}`} width="50" height="50" onError={(e) => handleImageError(e)} />
-                            <button onClick={() => handleImageDelete(index)}>Xoá</button>
-                        </div>
-                    ))}
-                    <button onClick={handleImageAdd}>Thêm ảnh</button>
+            <div className={cx('main-container')}>
+                <div className={cx('left-container')}>
+                    <label className={cx('form-control')}>
+                        Tên sản phẩm:
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    </label>
+                    <label className={cx('form-control')}>
+                        Giá:
+                        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
+                    </label>
+                    <label className={cx('form-control')}>
+                        Số lượng:
+                        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+                    </label>
+                    <label className={cx('form-control')}>
+                        Mô tả:
+                        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+                    </label>
                 </div>
-            </label>
-            <label>
-                Thể loại:
-                <div>
-                    {category.map((cat, index) => (
-                        <div key={index}>
-                            <input
-                                type="text"
-                                value={cat}
-                                onChange={(e) => handleCategoryInputChange(index, e)}
-                            />
-                            <button onClick={() => handleCategoryDelete(index)}>Xoá</button>
+                <div className={cx('right-container')}>
+                    
+                        Ảnh:
+                        <div className={cx('add-image-wrapper')}>
+                            {images.map((image, index) => (
+                                <div className={cx('add-image-item')} key={index} >
+                                <label className={cx('form-control')} >
+                                    <img src={image} alt={`Product ${index + 1}`} width="50" height="50" onError={(e) => handleImageError(e)} />
+                                    <input
+                                        type="text"
+                                        value={image}
+                                        onChange={(e) => handleImageInputChange(index, e)}
+                                    />
+                                    <button className={cx('remove-btn')} onClick={() => handleImageDelete(index)}>Xoá</button>
+                                </label>
+                                </div>
+                            ))}
+                            <button className={cx('btn-add')} onClick={handleImageAdd}><MdAdd/> <p>Thêm</p></button>
                         </div>
-                    ))}
-                    <button onClick={handleCategoryAdd}>Thêm thể loại</button>
+                    
+                    <label className={cx('form-control')}>
+                        Thể loại:
+                        <div className={cx('category-container')}>
+                            {category.map((cat, index) => (
+                                <div className={cx('category-item')} key={index}>
+                                    {/* <input
+                                    type="text"
+                                    value={cat}
+                                    onChange={(e) => handleCategoryInputChange(index, e)}
+                                /> */}
+
+                                    <select value={cat} onChange={(e) => handleCategorySelectChange(index, e)}>
+                                        <option value="">Chọn thể loại</option>
+                                        <option value="Cắm trại, leo núi">Cắm trại, leo núi</option>
+                                        <option value="Balo & túi du lịch">Balo & túi du lịch</option>
+                                        <option value="Giày dép">Giày dép</option>
+                                        <option value="Đồ phượt xe">Đồ phượt xe</option>
+                                        <option value="Đồ điện tử công nghệ">Đồ điện tử công nghệ</option>
+                                        <option value="Dao và dụng cụ">Dao và dụng cụ</option>
+                                    </select>
+                                    <button className={cx('remove-btn')} onClick={() => handleCategoryDelete(index)}>Xoá</button>
+                                </div>
+                            ))}
+                            <button className={cx('btn-add')} onClick={handleCategoryAdd}><MdAdd/> <p>Thêm</p> </button>
+                        </div>
+                    </label>
+                    <label className={cx('form-control')}>
+                        Giảm giá (từ 0 - 1):
+                        <input type="number" value={discount} onChange={(e) => setDiscount(e.target.value)} />
+                    </label>
                 </div>
-            </label>
-            <label>
-                Giảm giá:
-                <input type="number" value={discount} onChange={(e) => setDiscount(e.target.value)} />
-            </label>
-            <button onClick={handleSave}>Lưu</button>
+            </div>
+
+            <div className={cx('footer-container')}>
+            <button className={cx('save-btn')} onClick={handleSave}>Lưu</button>
+            </div>
         </div>
     );
 }
